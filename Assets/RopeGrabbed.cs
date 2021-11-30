8777db5879612abfa2ee4154966fde8c367dae06
Micha <Micha@fh.de>
2021-11-30 17:44:56 +0100

    Rope

---
+++ Assets/RopeGrabbed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeGrabbed : MonoBehaviour
+{
+    public void ropeIsGrabbed()
+    {
+        Debug.Log("Hallo");
+    }
+}
