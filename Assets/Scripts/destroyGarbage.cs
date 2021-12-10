bbe48326d84ac06a41fbcfae7ccbdb89e82db368
Micha <Micha@FH.de>
2021-12-10 14:25:46 +0100

    Destroyed Garbage Object when selected

---
+++ Assets/Scripts/destroyGarbage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyGarbage : MonoBehaviour
+{
+  public void destroy()
+    {
+        Destroy(gameObject);
+    }
+}
