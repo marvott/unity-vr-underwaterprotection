afc1f04648f569d195a74b48df6519beba3a3f1e
Micha <Micha@Fh.de>
2021-12-21 16:55:15 +0100

    Version 0.1 pre Ghost

--- Assets/Scripts/dragMovementSystem.cs
+++ Assets/Scripts/dragMovementSystem.cs
@@ -4,13 +4,12 @@
  *button. Then he can drag himself just like he would do in the real world along the rope. 
  ***************************************************************************************************************************/
 
-//ToDo: Weitere Kommentare einfügen.
 namespace UnityEngine.XR.Interaction.Toolkit
 {
     public class dragMovementSystem : ContinuousMoveProviderBase
     {
         public GameObject SceneManager;
-        public ActionBasedController controller;
+        public ActionBasedController controller; //LeftHand Controller.
         public Camera camera;
         private Vector3 controllerPos, camViewVec;
         private Vector2 movementVec;
@@ -26,7 +25,7 @@ namespace UnityEngine.XR.Interaction.Toolkit
             camViewVec = camera.transform.forward;
             controllerPos= controller.positionAction.action.ReadValue<Vector3>();
             deltaZ = controllerPos.z - prevZPos;
-            base.moveSpeed = -deltaZ * 100;
+            base.moveSpeed = -deltaZ * 100; //moveSpeed dertermines the direction and speed of drag Movement.
             if (Mathf.Abs(controllerPos.z - prevZPos) >= 0.005)
             {
                 controllerMoved = true;
@@ -40,14 +39,14 @@ namespace UnityEngine.XR.Interaction.Toolkit
             if (SceneManager.GetComponent<SceneManagerScript>().isRopeTouched() && controllerMoved)
             {
 
-                movementVec = new Vector2(0 - camViewVec.x, 1 + (camViewVec.z -1));
+                movementVec = new Vector2(0 - camViewVec.x, 1 + (camViewVec.z -1)); //movementVec is always interpreted according to the players viewing angle. To always get a Vector (0,1) in the world coordinate system the viewing angle is computet corresponding.
                 Debug.Log("CamVec" + camViewVec);
                 Debug.Log("MoveVec" + movementVec);
                 return movementVec;
             }
             else
             {
-                movementVec = new Vector2(0, 0);
+                movementVec = new Vector2(0, 0); //if the rope is not touched or the controller is not moved there is no drag Movement.
                 return movementVec;
             }
         }
