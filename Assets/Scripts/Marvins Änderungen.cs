3c9b55e144f2b405b3548c64da917710a8079f54
Marvin Ottersberg <marvin.ottersberg@student.fh-kiel.de>
2021-12-31 17:45:32 +0100

    Wal folgt pfad, 2 assets hinzugefuegt und wasseroberfläche

--- Assets/Scripts/Marvins Änderungen.cs
+++ Assets/Scripts/Marvins Änderungen.cs
@@ -2,9 +2,27 @@
  * 
  * 
  * Alle 'Änderungen die marvin gemacht hat
+ * 
  * Ordner in Skripte ordner erstellt
  * 
  * XRInteractor Farbe des Rays auf durchsichtig
+ * 
+ * Vacuum Cleaner als Prefab erstellt. vllt nicht mehr richig angeordnet im game view -> Überprüfen
+ * 
+ * ordner und Hierarchie-Struktur erstellt
+ * 
+ * 
+ * standard assets aus unitystore hinzugefügt: nur die wasser unter environment sachen
+ * 
+ * water4advanced hinzugefügt und in water ordner gepackt -> ist in surrounding
+ * 
+ * 
+ * surface materials aus standardassets für den boden nehmen?
+ * 
+ * bezier path creator als asset: https://assetstore.unity.com/packages/tools/utilities/b-zier-path-creator-136082
+ * 
+ * wal folgt path
+ * 
  * 
  * 
  * */
