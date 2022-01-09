1f11de0be16040f4616c2144299d9fed3aba21c5
Micha <Micha@Fh.de>
2022-01-09 16:06:19 +0100

    working

--- Assets/Scripts/Objects/WhaleInView.cs
+++ Assets/Scripts/Objects/WhaleInView.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PathCreation.Examples;
+using System;
 
 public class WhaleInView : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class WhaleInView : MonoBehaviour
     public GameObject whale;
     private Vector3 camViewVec, playerPosition, whalePosition, whalePlayerVec;
     private float playerWhaleHighestValue, camHighestValue;
+    public float tolerance, triggerDistanceZ;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,31 +24,35 @@ public class WhaleInView : MonoBehaviour
         playerPosition = gameObject.transform.position;
         whalePosition = whale.transform.position;
         whalePlayerVec = new Vector3(whalePosition.x - playerPosition.x, whalePosition.y - playerPosition.y, whalePosition.z - playerPosition.z);
-        if(whalePlayerVec.x > whalePlayerVec.y)
+
+        playerWhaleHighestValue = whalePlayerVec.y;
+        if(Math.Abs(whalePlayerVec.x) > Math.Abs(whalePlayerVec.y))
         {
             playerWhaleHighestValue = whalePlayerVec.x;
         }
 
-        if(whalePlayerVec.z > playerWhaleHighestValue)
+        if(Math.Abs(whalePlayerVec.z) > Math.Abs(playerWhaleHighestValue))
         {
             playerWhaleHighestValue = whalePlayerVec.z;
         }
 
         whalePlayerVec = new Vector3(whalePlayerVec.x / playerWhaleHighestValue, whalePlayerVec.y / playerWhaleHighestValue, whalePlayerVec.z / playerWhaleHighestValue);
 
-        if (camViewVec.x > camViewVec.y)
+        camHighestValue = camViewVec.y;
+        if (Math.Abs(camViewVec.x) > Math.Abs(camViewVec.y))
         {
             camHighestValue = camViewVec.x;
         }
 
-        if (camViewVec.z > camHighestValue)
+        if (Math.Abs(camViewVec.z) > Math.Abs(camHighestValue))
         {
             camHighestValue = camViewVec.z;
         }
 
         camViewVec = new Vector3(camViewVec.x / camHighestValue, camViewVec.y / camHighestValue, camViewVec.z / camHighestValue);
         Debug.Log("whalePlayerVec " + whalePlayerVec + "camViewVec" + camViewVec);
-        if((whalePlayerVec.x >= camViewVec.x-0.3 && whalePlayerVec.x <= camViewVec.x + 0.3) && (whalePlayerVec.y >= camViewVec.y - 0.3 && whalePlayerVec.y <= camViewVec.y + 0.3) && (whalePlayerVec.z >= camViewVec.z - 0.3 && whalePlayerVec.z <= camViewVec.z + 0.3))
+        Debug.Log("PlayerPosition: " + playerPosition.z + "Objektposition" + whalePosition.z);
+        if((whalePlayerVec.x >= camViewVec.x-tolerance && whalePlayerVec.x <= camViewVec.x + tolerance) && (whalePlayerVec.y >= camViewVec.y - tolerance && whalePlayerVec.y <= camViewVec.y + tolerance) && (whalePlayerVec.z >= camViewVec.z - tolerance && whalePlayerVec.z <= camViewVec.z + tolerance) && Math.Abs(playerPosition.z - whalePosition.z) <= triggerDistanceZ)
         {
             whale.GetComponent<PathFollower>().enabled = true;
         }
