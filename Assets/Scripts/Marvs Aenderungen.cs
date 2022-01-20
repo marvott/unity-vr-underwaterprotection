c213ce82318716f2f25cedbef02c7344152225a3
marv <marv@fh.de>
2022-01-20 15:56:14 +0100

    Endgame Script

---
+++ Assets/Scripts/Marvs Aenderungen.cs
@@ -0,0 +1,16 @@
+//what did marvin do
+
+/*
+ * 
+ * endgame script
+ *  Am Ende muss dafür noch dierichtige Taste hinzugefügt werden
+ *  wal und xrrig und amountpieces wird auf urpsrung gesetzt
+ * terrain verändert
+ * asset vom holz und die textur auf nem text im canvas
+ * fontordner in external ordner erstellt
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
