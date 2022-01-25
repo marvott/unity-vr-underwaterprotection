db1e87b91dcd91fb1175ed4a839fda963f919a75
marv <marv@fh.de>
2022-01-25 19:52:46 +0100

    Objektliste für Rocks und Garbages

--- Assets/Scripts/Marvs Aenderungen.cs
+++ Assets/Scripts/Marvs Aenderungen.cs
@@ -3,14 +3,26 @@
 /*
  * 
  * endgame script
- *  Am Ende muss dafür noch dierichtige Taste hinzugefügt werden
+ *          MERGE TODO:
+ *          Am Ende muss dafür noch die richtige Taste hinzugefügt werden
+ *          und ohi script aus dem scene manager einfügen
  *  wal und xrrig und amountpieces wird auf urpsrung gesetzt
  * terrain verändert
- * asset vom holz und die textur auf nem text im canvas
+ * asset vom holz und die textur auf nem text im canvas (https://assetstore.unity.com/packages/2d/textures-materials/wood/hand-painted-seamless-wood-texture-vol-6-162145)
  * fontordner in external ordner erstellt
  * anfangsschild mit steuerungs text als prefab
- * wal asset eingefügt -> in external assets drin
- * Cola Dose als Garbage Prefab im Ordner External Assets -> External Garbages
+ * wal asset eingefügt -> in external assets drin (https://assetstore.unity.com/packages/3d/characters/animals/fish/humpback-whale-3547)
+ * Cola Dose als Garbage Prefab im Ordner External Assets -> External Garbages (https://assetstore.unity.com/packages/3d/cola-can-96659)
+ *sardinien (https://assetstore.unity.com/packages/3d/characters/animals/fish/sardine-37963)
  * 
  * 
+ * hand assets:https://github.com/Novaborn-dev/VR-Hands-with-Unity-XR
+ *  video für hand: https://www.youtube.com/watch?v=DVAsL6sGmlc (alternative muss ich noch ausschecken: https://www.youtube.com/watch?v=ihG5nE0thW0)
+ *  
+ *  seil dünner gemacht
+ *  
+ *  spawnen von objekten aus einer liste: Garbages und Rocks
+ *  Rocks: https://assetstore.unity.com/packages/3d/props/exterior/pbr-rock-cliffs-pack-105772
+ *  mugs liste: https://assetstore.unity.com/packages/3d/props/interior/plates-bowls-mugs-pack-146682
+ *  ashtrays: https://assetstore.unity.com/packages/3d/props/interior/ashtrays-140632
  */
