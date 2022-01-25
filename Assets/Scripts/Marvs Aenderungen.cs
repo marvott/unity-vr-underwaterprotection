73068fa1a672aeb09f3b681aaf1e630371448b5b
marv <marv@fh.de>
2022-01-25 16:44:09 +0100

    wal als asset und schild

--- Assets/Scripts/Marvs Aenderungen.cs
+++ Assets/Scripts/Marvs Aenderungen.cs
@@ -8,9 +8,9 @@
  * terrain verändert
  * asset vom holz und die textur auf nem text im canvas
  * fontordner in external ordner erstellt
- * 
- * 
- * 
+ * anfangsschild mit steuerungs text als prefab
+ * wal asset eingefügt -> in external assets drin
+ * Cola Dose als Garbage Prefab im Ordner External Assets -> External Garbages
  * 
  * 
  */
