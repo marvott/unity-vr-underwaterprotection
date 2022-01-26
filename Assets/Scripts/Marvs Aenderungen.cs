261433281b132c83c0550c7c577f68dbc859c203
Marvin Ottersberg <marvin.ottersberg@student.fh-kiel.de>
2022-01-26 19:10:49 +0100

    technical docu and assets for garbage

--- Assets/Scripts/Marvs Aenderungen.cs
+++ Assets/Scripts/Marvs Aenderungen.cs
@@ -4,25 +4,23 @@
  * 
  * endgame script
  *          MERGE TODO:
- *          Am Ende muss dafür noch die richtige Taste hinzugefügt werden
- *          und ohi script aus dem scene manager einfügen
+ *          Am Ende muss daf?r noch die richtige Taste hinzugef?gt werden
+ *          und ohi script aus dem scene manager einf?gen
  *  wal und xrrig und amountpieces wird auf urpsrung gesetzt
- * terrain verändert
- * asset vom holz und die textur auf nem text im canvas (https://assetstore.unity.com/packages/2d/textures-materials/wood/hand-painted-seamless-wood-texture-vol-6-162145)
+ * terrain ver?ndert
+ * asset vom holz und die textur auf nem text im canvas
  * fontordner in external ordner erstellt
  * anfangsschild mit steuerungs text als prefab
- * wal asset eingefügt -> in external assets drin (https://assetstore.unity.com/packages/3d/characters/animals/fish/humpback-whale-3547)
- * Cola Dose als Garbage Prefab im Ordner External Assets -> External Garbages (https://assetstore.unity.com/packages/3d/cola-can-96659)
- *sardinien (https://assetstore.unity.com/packages/3d/characters/animals/fish/sardine-37963)
- * 
- * 
- * hand assets:https://github.com/Novaborn-dev/VR-Hands-with-Unity-XR
- *  video für hand: https://www.youtube.com/watch?v=DVAsL6sGmlc (alternative muss ich noch ausschecken: https://www.youtube.com/watch?v=ihG5nE0thW0)
+ * wal asset eingef?gt -> in external assets drin (
+ * Cola Dose als Garbage Prefab im Ordner External Assets -> External Garbages (
+
+ *  video f?r hand: https://www.youtube.com/watch?v=DVAsL6sGmlc (alternative muss ich noch ausschecken: https://www.youtube.com/watch?v=ihG5nE0thW0)
  *  
- *  seil dünner gemacht
+ *  seil d?nner gemacht
  *  
  *  spawnen von objekten aus einer liste: Garbages und Rocks
- *  Rocks: https://assetstore.unity.com/packages/3d/props/exterior/pbr-rock-cliffs-pack-105772
- *  mugs liste: https://assetstore.unity.com/packages/3d/props/interior/plates-bowls-mugs-pack-146682
- *  ashtrays: https://assetstore.unity.com/packages/3d/props/interior/ashtrays-140632
+ *
+ *  
+ *  
+ *  
  */
